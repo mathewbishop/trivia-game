77f8e737684987e4017d3977179941ac3000f685
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-09-07 17:03:00 -0500

    initial site files

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+// trivia questions (forms) with multiple choice or true/false
+// limited amount of time
+// game ends when time runs out, questions answered correctly/incorrectly is displayed
+// cannot pick more than one answer per question
+// countdown timer displayed

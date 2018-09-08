3c7d7c17c782b2be1d3f4cbe2906fca5d7e52fee
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-09-08 18:37:26 -0500

    markup and style cover page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,22 @@
-// trivia questions (forms) with multiple choice or true/false
-// limited amount of time
-// game ends when time runs out, questions answered correctly/incorrectly is displayed
+// trivia questions (forms?) with multiple choice or true/false
+// limited amount of time for each question
+// if time runs out, correct answer is displayed for that question
+    // without user input, after a few seconds, next question starts
 // cannot pick more than one answer per question
 // countdown timer displayed
 // one question displayed at a time
+// after last question display grade and option to start over
+
+
+// we need
+    // question data (list of questions and answers)
+        // object literals for questions (an array of them?)
+    // a 'start' button that begins the game
+    // a 'start over' button at the end of the game that restarts game
+    // a timer for each question
+    // a right/wrong display for each question
+    // a grade display at end of game
+    // a method to cycle through questions
+
+
+

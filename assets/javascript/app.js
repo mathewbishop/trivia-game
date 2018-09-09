49f9934a2837660a0e711d491e100a0acdbd2214
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-09-08 23:25:17 -0500

    start app. create two objects which contain trivia questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,5 +18,24 @@
     // a grade display at end of game
     // a method to cycle through questions
 
+var parisGun = {
+    question: "True or False: 'The Paris Gun' was a German artillery piece that was able to hit Paris from its position behind the Western Front's German line (about 75 miles).",
+    answers: {
+        a: "True",
+        b: "False"
+    },
+    correctAnswer: "True",
+    moreInfo: "'The Paris Gun' had to fire successively larger shells each time it was fired due to the barrel diameter increasing with each shot. The shells fired by this gun reached the greatest height of any human-made projectile prior to first successful v-2 rocket flight test in 1942"
+};
+
+var poisonGas = {
+    question: "True or False: The use of poison gas was technically a war crime during World War I.",
+    answers: {
+        a: "True",
+        b: "False"
+    },
+    correctAnswer: "True",
+    moreInfo: "The use of poision gas violated the agreed upon rules of war outlined in the 1899 Hague Convention"
+};
 
 

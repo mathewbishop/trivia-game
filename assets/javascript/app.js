bc9296e703cdaf70315cc9438b8ce9f26151e68b
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-09-10 15:53:09 -0500

    pseudo code restarting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,23 @@
-// trivia questions (forms?) with multiple choice or true/false
-// limited amount of time for each question
-// if time runs out, correct answer is displayed for that question
-    // without user input, after a few seconds, next question starts
-// cannot pick more than one answer per question
-// countdown timer displayed
-// one question displayed at a time
-// after last question display grade and option to start over
 
-
-// we need
-    // question data (list of questions and answers)    √
-        // object literals for questions (an array of them?)    √
-    // user input to choose answers for quiz    √
-    // a 'start' button that begins the game    √
-    // a 'start over' button at the end of the game that restarts game
-    // a timer for each question
-    // a right/wrong display for each question
-    // a grade display at end of game
-    // a method to cycle through questions (on click or timeout)
 
 var questionDiv = $('#question-div');
 var answerDiv = $('#answer-div');
 var trueBtn = $('#true-btn');
 var falseBtn = $('#false-btn');
  
-var questionOne = {
+var questions = [{
     question: "True or False: 'The Paris Gun' was a German artillery piece that was able to hit Paris from its position behind the Western Front's German line (about 75 miles distance).",
     correctAnswer: "True",
     moreInfo: "'The Paris Gun' had to fire successively larger shells each time it was fired due to the barrel diameter increasing with each shot. The shells fired by this gun reached the greatest height of any human-made projectile prior to first successful v-2 rocket flight test in 1942"
-}
-
-var question2 = {
+},
+{
     question: "True or False: The use of poison gas was technically a war crime during World War I.",
     correctAnswer: "True",
     moreInfo: "The use of poision gas violated the agreed upon rules of war outlined in the 1899 Hague Convention"
-}
+}]
+
+
+
 
 
 

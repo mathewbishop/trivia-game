5ef5e787b778f198708dcd5434e95eb3976471e5
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-09-11 10:59:38 -0500

    correct answers object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,40 @@
+// data for later
+    //'The Paris Gun' had to fire successively larger shells each time it was fired due to the barrel diameter increasing with each shot. The shells fired by this gun reached the greatest height of any human-made projectile prior to first successful v-2 rocket flight test in 1942"
+    //"The use of poision gas violated the agreed upon rules of war outlined in the 1899 Hague Convention"
+
+// features
+    // timer 
+    // grading function
+        // check correct answers against user answers
+    // grade page
+    // restart button
+//"1", "1", "4", "2", "1", "1", "1", "4", "3", "1"
+var correctAnswers = {
+    answer1: "1",
+    answer2: "1",
+    answer3: "4",
+    answer4: "2",
+    answer5: "1",
+    answer6: "1",
+    answer7: "1",
+    answer8: "4",
+    answer9: "3",
+    answer10: "1"
+}
+var userAnswers = [];
 
 
-var questionDiv = $('#question-div');
-var answerDiv = $('#answer-div');
-var trueBtn = $('#true-btn');
-var falseBtn = $('#false-btn');
+
+
+
+
  
-var questions = [{
-    question: "True or False: 'The Paris Gun' was a German artillery piece that was able to hit Paris from its position behind the Western Front's German line (about 75 miles distance).",
-    correctAnswer: "True",
-    moreInfo: "'The Paris Gun' had to fire successively larger shells each time it was fired due to the barrel diameter increasing with each shot. The shells fired by this gun reached the greatest height of any human-made projectile prior to first successful v-2 rocket flight test in 1942"
-},
-{
-    question: "True or False: The use of poison gas was technically a war crime during World War I.",
-    correctAnswer: "True",
-    moreInfo: "The use of poision gas violated the agreed upon rules of war outlined in the 1899 Hague Convention"
-}]
+
+
+
+    
+
+
 
 
 

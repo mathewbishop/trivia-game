435aa51a998e40df6785bd10c251acd3274f03a8
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-09-12 11:19:55 -0500

    starting timer mechanism

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,7 @@
-// data for later
-    //'The Paris Gun' had to fire successively larger shells each time it was fired due to the barrel diameter increasing with each shot. The shells fired by this gun reached the greatest height of any human-made projectile prior to first successful v-2 rocket flight test in 1942"
-    //"The use of poision gas violated the agreed upon rules of war outlined in the 1899 Hague Convention"
-
-// features
-    // timer 
-    // grading mechanism
-        // check correct answers against user answers
-            // get value of the answer user picked
-            // check that value against the correct answer
-    // grade page
-    // restart button
-//"1", "1", "4", "2", "1", "1", "1", "4", "3", "1"
+// need timer
 
 
-//var correctAnswers = ["1", "1", "4", "2", "1", "1", "1", "4", "3", "1"];
-
+// object storing correct answers
 var correctAnswers = {
     answer0: "1",
     answer1: "1",
@@ -28,11 +15,12 @@ var correctAnswers = {
     answer9: "1"
 }
 
+// global var list
 var userCorrect = 0;
 var userIncorrect = 0;
 var userUnanswered = 0;
 
-
+// function resets quiz at end
 function resetQuiz() {
     userCorrect = 0;
     userIncorrect = 0;
@@ -44,12 +32,14 @@ function resetQuiz() {
     $('main').show();
 }
 
-
+// starts quiz 
 $('#start-btn').click(function() {
     $('header').hide();
     $('main').show();
 })
 
+
+// runs grading logic on submit button click
 $('#submit-btn').click(function() {
     
     if ($('input[name="question0"]').is(":checked")) {
@@ -197,9 +187,11 @@ $('#submit-btn').click(function() {
         userUnanswered++;
     }
 
+    // switches page to grading data on submit
     $('main').hide();
     $('#grade-div').show();
     
+    // displays grade data
     $('#correct-total').html("Correct: " + userCorrect);
     $('#incorrect-total').html("Incorrect: " + userIncorrect);
     $('#unanswered-total').html("Unanswered: " + userUnanswered);
@@ -210,6 +202,166 @@ $('#submit-btn').click(function() {
         
 
 })
-    
+
+// resets quiz on click
 $('#reset-btn').click(resetQuiz);
 
+function timeLimit() {
+    if ($('input[name="question0"]').is(":checked")) {
+
+        if ($('input[name="question0"]:checked').val() === (correctAnswers.answer0)) {
+            userCorrect++;
+        } 
+        if ($('input[name="question0"]:checked').val() != (correctAnswers.answer0)) { 
+            userIncorrect++;
+        }
+    }
+    else {
+        userUnanswered++;
+    }
+
+
+    if ($('input[name="question1"]').is(":checked")) {
+
+        if ($('input[name="question1"]:checked').val() === (correctAnswers.answer1)) {
+            userCorrect++;
+        } 
+        if ($('input[name="question1"]:checked').val() != (correctAnswers.answer1)) { 
+            userIncorrect++;
+        }
+    }
+    else {
+        userUnanswered++;
+    }
+
+
+    if ($('input[name="question2"]').is(":checked")) {
+
+        if ($('input[name="question2"]:checked').val() === (correctAnswers.answer2)) {
+            userCorrect++;
+        } 
+        if ($('input[name="question2"]:checked').val() != (correctAnswers.answer2)) { 
+            userIncorrect++;
+        }
+    }
+    else {
+        userUnanswered++;
+    }
+
+
+    if ($('input[name="question3"]').is(":checked")) {
+
+        if ($('input[name="question3"]:checked').val() === (correctAnswers.answer3)) {
+            userCorrect++;
+        } 
+        if ($('input[name="question3"]:checked').val() != (correctAnswers.answer3)) { 
+            userIncorrect++;
+        }
+    }
+    else {
+        userUnanswered++;
+    }
+
+
+
+    if ($('input[name="question4"]').is(":checked")) {
+
+        if ($('input[name="question4"]:checked').val() === (correctAnswers.answer4)) {
+            userCorrect++;
+        } 
+        if ($('input[name="question4"]:checked').val() != (correctAnswers.answer4)) { 
+            userIncorrect++;
+        }
+    }
+    else {
+        userUnanswered++;
+    }
+
+
+
+    if ($('input[name="question5"]').is(":checked")) {
+
+        if ($('input[name="question5"]:checked').val() === (correctAnswers.answer5)) {
+            userCorrect++;
+        } 
+        if ($('input[name="question5"]:checked').val() != (correctAnswers.answer5)) { 
+            userIncorrect++;
+        }
+    }
+    else {
+        userUnanswered++;
+    }
+
+
+
+    if ($('input[name="question6"]').is(":checked")) {
+
+        if ($('input[name="question6"]:checked').val() === (correctAnswers.answer6)) {
+            userCorrect++;
+        } 
+        if ($('input[name="question6"]:checked').val() != (correctAnswers.answer6)) { 
+            userIncorrect++;
+        }
+    }
+    else {
+        userUnanswered++;
+    }
+
+
+
+    if ($('input[name="question7"]').is(":checked")) {
+
+        if ($('input[name="question7"]:checked').val() === (correctAnswers.answer7)) {
+            userCorrect++;
+        } 
+        if ($('input[name="question7"]:checked').val() != (correctAnswers.answer7)) { 
+            userIncorrect++;
+        }
+    }
+    else {
+        userUnanswered++;
+    }
+
+
+
+    if ($('input[name="question8"]').is(":checked")) {
+
+        if ($('input[name="question8"]:checked').val() === (correctAnswers.answer8)) {
+            userCorrect++;
+        } 
+        if ($('input[name="question8"]:checked').val() != (correctAnswers.answer8)) { 
+            userIncorrect++;
+        }
+    }
+    else {
+        userUnanswered++;
+    }
+
+
+
+    if ($('input[name="question9"]').is(":checked")) {
+
+        if ($('input[name="question9"]:checked').val() === (correctAnswers.answer9)) {
+            userCorrect++;
+        } 
+        if ($('input[name="question9"]:checked').val() != (correctAnswers.answer9)) { 
+            userIncorrect++;
+        }
+    }
+    else {
+        userUnanswered++;
+    }
+
+    // switches page to grading data on submit
+    $('main').hide();
+    $('#grade-div').show();
+    
+    // displays grade data
+    $('#correct-total').html("Correct: " + userCorrect);
+    $('#incorrect-total').html("Incorrect: " + userIncorrect);
+    $('#unanswered-total').html("Unanswered: " + userUnanswered);
+
+        console.log("correct " + userCorrect);
+        console.log("incorrect " + userIncorrect);
+        console.log("unanswered " + userUnanswered);
+}
